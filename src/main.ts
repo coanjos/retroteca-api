454712a141efe86014f7c18949713c6a93f9b159
Gustavo Oliveira dos Anjos <anjos.gu@gmail.com>
2021-09-10 20:32:10 -0300

    adiciona arquivos Dockerfile e Docker-Compose

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addTag('app')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/swagger', app, document);
 
   await app.listen(3000);
 }
