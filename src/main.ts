4f040f67b827c622033a9830fd8e12ad4419cacb
Caio dos Anjos <coa.anjos@gmail.com>
2021-12-22 14:27:14 -0300

    Cria resource de jogos

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +9,6 @@ async function bootstrap() {
     .setTitle('Retroteca')
     .setDescription('Uma aplicação para recomendar um jogo aleatório toda semana.')
     .setVersion('1.0')
-    .addTag('app')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger', app, document);
